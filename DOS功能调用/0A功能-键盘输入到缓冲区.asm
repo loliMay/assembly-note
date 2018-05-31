3de43f7eb06fea803253e3da24670808a91ab21a
LoliMay <1404363070@qq.com>
2018-05-31 11:39:25 +0800

    更新汇编赋值情况总结

--- DOS功能调用/0A功能-键盘输入到缓冲区.asm
+++ DOS功能调用/0A功能-键盘输入到缓冲区.asm
@@ -1,5 +1,5 @@
 DATAS SEGMENT
-    BUF DB 10 DUP($)			;开辟10个字节的空间
+    BUF DB 6 DUP('$')			;开辟6个字节的空间
 DATAS ENDS
 
 STACKS SEGMENT
@@ -12,11 +12,15 @@ START:
     MOV AX,DATAS
     MOV DS,AX
     LEA DX,BUF ;设置DX,使字符串首地址=DS:DX
-    MOV AH,0A
+    MOV BX,DX
+    MOV BYTE PTR[BX],5	;设置缓冲区字节数为0
+    MOV AH,0AH
     INT 21H
     MOV AH,4CH
     INT 21H
 CODES ENDS
     END START
 ;一个寄存器有两个字节
+
+
 
